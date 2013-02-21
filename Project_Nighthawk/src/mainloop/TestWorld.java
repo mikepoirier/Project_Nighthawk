0a6fb682ad3b3667a3a6e6490322635b3cd7ae18
poiri524 <poiri524@regis.edu>
2013-02-21 15:44:10 -0700

    Modified TestWorld

--- Project_Nighthawk/src/mainloop/TestWorld.java
+++ Project_Nighthawk/src/mainloop/TestWorld.java
@@ -44,10 +44,10 @@ public class TestWorld extends JFrame implements Runnable {
     public void init() {
         rc = ResourceCache.getInstance();
         af = ActorFactory.getInstance();
-        rc.setResourceLocation("../../Assets/Images/");
+        rc.setResourceLocation("C:\\Users\\Mike\\Documents\\NetBeansProjects\\Project_Nighthawk(Master)\\Assets\\Images");
         rc.upload("SMikeWalk.zip");
         rc.upload("SRobWalk.zip");
-        player = af.createActor("../../Assets/Actors/player_2D.xml");
+        player = af.createActor("C:/Users/Mike/Documents/NetBeansProjects/Project_Nighthawk(Master)/Assets/Actors/player_2D.xml");
         object = new Rectangle(275, 175, 25, 25);
     }
 
