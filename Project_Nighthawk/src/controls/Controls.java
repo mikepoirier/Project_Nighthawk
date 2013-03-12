ec7d4d1af7b46a57717d2ec5b06e9072271b933e
poiri524 <poiri524@regis.edu>
2013-03-12 15:10:01 -0600

    Added Contorls.java

---
+++ Project_Nighthawk/src/controls/Controls.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controls;
+
+import java.awt.event.KeyListener;
+import actors.*;
+
+/**
+ *
+ * @author Mike
+ */
+public class Controls
+{
+    
+}
