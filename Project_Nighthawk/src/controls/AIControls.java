117c5b54a696a7bd609f6b102584a9bb530558f1
poiri524 <poiri524@regis.edu>
2013-03-21 13:55:55 -0600

    Updated things.

--- Project_Nighthawk/src/controls/AIControls.java
+++ Project_Nighthawk/src/controls/AIControls.java
@@ -4,26 +4,14 @@
  */
 package controls;
 
-<<<<<<< HEAD:Project_Nighthawk/src/controls/Controls.java
-import java.awt.event.KeyListener;
-import actors.*;
-=======
-import actors.Actor;
->>>>>>> origin/Sandbox-MP:Project_Nighthawk/src/controls/AIControls.java
-
 /**
  *
  * @author Mike
  */
-<<<<<<< HEAD:Project_Nighthawk/src/controls/Controls.java
-public class Controls
-{
     
-=======
 public class AIControls extends BaseControls
 {
     public AIControls()
     {
     }
->>>>>>> origin/Sandbox-MP:Project_Nighthawk/src/controls/AIControls.java
 }
